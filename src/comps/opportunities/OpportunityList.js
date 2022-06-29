58f323b611d25784c5356c488f46ba23e119d674
David Chen <dchen07@protonmail.com>
2022-06-28 23:16:30 -0400

    Add explicit message when listing 0 opportunities
    Adds a check for no opportunities, and presents a message if that is the
    case.

--- src/comps/opportunities/OpportunityList.js
+++ src/comps/opportunities/OpportunityList.js
@@ -14,23 +14,28 @@ import OpportunityCard from "./OpportunityCard";
 	tags: [String], optional
 */
 const OpportunityList = (data) => {
-  return (
-    <>
-      {data?.opportunities?.opportunities?.map((opportunity) => (
-        <OpportunityCard
-          key={opportunity.id}
-          title={opportunity.title}
-          date={opportunity?.date}
-          description={opportunity.description}
-          appDeadline={opportunity?.appDeadline}
-          cost={opportunity?.cost}
-          location={opportunity?.location}
-          link={opportunity?.link}
-          tags={opportunity?.tags}
-        />
-      ))}
-    </>
-  );
+  if (data?.opportunities?.opportunities.length) {
+    return (
+        <>
+          {data?.opportunities?.opportunities?.map((opportunity) => (
+              <OpportunityCard
+                  key={opportunity.id}
+                  title={opportunity.title}
+                  date={opportunity?.date}
+                  description={opportunity.description}
+                  appDeadline={opportunity?.appDeadline}
+                  cost={opportunity?.cost}
+                  location={opportunity?.location}
+                  link={opportunity?.link}
+                  tags={opportunity?.tags}
+              />
+          ))}
+        </>
+    );
+  }
+  else {
+    return (<h1>No opportunities found.</h1>)
+  }
 };
 
 export default OpportunityList;
