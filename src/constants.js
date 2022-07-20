8cf0334963471d5842bb66f52ec248755a4211b4
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2022-07-20 15:02:45 -0400

    HTTPS

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const GRAPHQL_URI = process.env.REACT_APP_GRAPHQL_URI || "http://opportunities-api.stuysu.org/graphql"; // if running db on local, use http://localhost:3001/graphql
+export const GRAPHQL_URI = process.env.REACT_APP_GRAPHQL_URI || "https://opportunities-api.stuysu.org/graphql"; // if running db on local, use http://localhost:3001/graphql
 
 export const GOOGLE_LOGIN_CLIENT_ID = "979775004862-gg66eb6tialnpivrvsjmq7j7hcosths7.apps.googleusercontent.com";
