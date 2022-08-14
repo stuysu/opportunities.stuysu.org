58c1ec2bc9fd8b870d571bc81630ab753a5ec749
YBenPan <ybenp8104@outlook.com>
2022-08-13 22:37:38 -0400

    Revise TextField components

--- src/comps/opportunities/OpportunityForm.js
+++ src/comps/opportunities/OpportunityForm.js
@@ -10,12 +10,16 @@ const OpportunityForm = (opportunity = {}) => {
   const [cost, setCost] = React.useState(opportunity.cost || "");
   const [location, setLocation] = React.useState(opportunity.location || "");
   const [link, setLink] = React.useState(opportunity.link || "");
+  const [description, setDescription] = React.useState(
+    opportunity.description || ""
+  );
   return (
     <div>
-      <Grid container spacing={1}>
+      <Grid container spacing={2}>
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
           <TextField
             variant={"outlined"}
+            fullWidth
             label={"Title"}
             value={title}
             placeholder={"Summer Youth Employment Program"}
@@ -25,6 +29,7 @@ const OpportunityForm = (opportunity = {}) => {
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
           <TextField
             variant={"outlined"}
+            fullWidth
             label={"Date"}
             value={date}
             placeholder={"June 20 - August 12"}
@@ -34,6 +39,7 @@ const OpportunityForm = (opportunity = {}) => {
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
           <TextField
             variant={"outlined"}
+            fullWidth
             label={"Deadline"}
             value={appDeadline}
             placeholder={"2022-05-06"}
@@ -43,6 +49,7 @@ const OpportunityForm = (opportunity = {}) => {
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
           <TextField
             variant={"outlined"}
+            fullWidth
             label={"Cost"}
             value={cost}
             placeholder={"0"}
@@ -52,6 +59,7 @@ const OpportunityForm = (opportunity = {}) => {
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
           <TextField
             variant={"outlined"}
+            fullWidth
             label={"Location"}
             value={location}
             placeholder={"New York City"}
@@ -61,21 +69,25 @@ const OpportunityForm = (opportunity = {}) => {
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
           <TextField
             variant={"outlined"}
+            fullWidth
             label={"Link"}
             value={link}
             placeholder={"https://application.nycsyep.com/"}
             onChange={(e) => setLink(e.target.value)}
           />
         </Grid>
-        <Grid item>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
           <TextField
             variant={"outlined"}
-            fullwidth
+            fullWidth
             multiline
             maxRows={10}
+            label={"Description"}
+            value={description}
             placeholder={
               "Summer Youth Employment Program (SYEP) is the nation’s largest youth employment program, connecting NYC youth between the ages of 14 and 24 with career exploration opportunities and paid work experiences each summer. "
             }
+            onChange={(e) => setDescription(e.target.value)}
           />
         </Grid>
       </Grid>
--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { Grid } from "@mui/material";
+import { Box } from "@mui/material";
 import OpportunityForm from "../comps/opportunities/OpportunityForm";
 
 const Admin = () => {
@@ -9,13 +9,12 @@ const Admin = () => {
       <Helmet>
         <title>Admin</title>
       </Helmet>
-      <Grid container>
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <OpportunityForm>
 
-          </OpportunityForm>
-        </Grid>
-      </Grid>
+      <Box>
+        <OpportunityForm>
+          
+        </OpportunityForm>
+      </Box>
     </div>
   );
 };
