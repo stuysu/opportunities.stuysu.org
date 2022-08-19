aec07e48c3caf44ebb10ad82b2e49a8dc1249a88
YBenPan <ybenp8104@outlook.com>
2022-08-19 17:01:42 -0400

    Create new grid containers to reshuffle components

--- src/comps/opportunities/OpportunityForm.js
+++ src/comps/opportunities/OpportunityForm.js
@@ -51,83 +51,106 @@ const OpportunityForm = (opportunity = {}) => {
 
   return (
     <div>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <TextField
-            variant={"outlined"}
-            fullWidth
-            label={"Title"}
-            value={title}
-            placeholder={"Summer Youth Employment Program"}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+      <Grid container spacing={2} alignItems="stretch">
+        <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+              <TextField
+                variant={"outlined"}
+                fullWidth
+                label={"Title"}
+                value={title}
+                placeholder={"Summer Youth Employment Program"}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+              <TextField
+                variant={"outlined"}
+                fullWidth
+                label={"Date"}
+                value={date}
+                placeholder={"June 20 - August 12"}
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+              <TextField
+                variant={"outlined"}
+                fullWidth
+                label={"Deadline"}
+                value={appDeadline}
+                placeholder={"2022-05-06"}
+                onChange={(e) => setAppDeadline(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+              <TextField
+                variant={"outlined"}
+                fullWidth
+                label={"Cost"}
+                value={cost}
+                placeholder={"0"}
+                onChange={(e) => {
+                  setCost(e.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+              <TextField
+                variant={"outlined"}
+                fullWidth
+                label={"Location"}
+                value={location}
+                placeholder={"New York City"}
+                onChange={(e) => setLocation(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+              <TextField
+                variant={"outlined"}
+                fullWidth
+                label={"Link"}
+                value={link}
+                placeholder={"https://application.nycsyep.com/"}
+                onChange={(e) => setLink(e.target.value)}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+              <TextField
+                variant={"outlined"}
+                fullWidth
+                multiline
+                maxRows={10}
+                label={"Description"}
+                value={description}
+                sx={{ marginBottom: "10px" }}
+                placeholder={
+                  "Summer Youth Employment Program (SYEP) is the nation’s largest youth employment program, connecting NYC youth between the ages of 14 and 24 with career exploration opportunities and paid work experiences each summer. "
+                }
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <TextField
-            variant={"outlined"}
-            fullWidth
-            label={"Date"}
-            value={date}
-            placeholder={"June 20 - August 12"}
-            onChange={(e) => setDate(e.target.value)}
-          />
+        <Grid item xs={6} sm={6} md={2} lg={2} xl={2}>
+          <Grid style={{ height: "100%" }}>
+            <TextField
+              fullWidth
+              label={"Categories"}
+              style={{ height: "100%" }}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <TextField
-            variant={"outlined"}
-            fullWidth
-            label={"Deadline"}
-            value={appDeadline}
-            placeholder={"2022-05-06"}
-            onChange={(e) => setAppDeadline(e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <TextField
-            variant={"outlined"}
-            fullWidth
-            label={"Cost"}
-            value={cost}
-            placeholder={"0"}
-            onChange={(e) => {
-              setCost(e.target.value);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <TextField
-            variant={"outlined"}
-            fullWidth
-            label={"Location"}
-            value={location}
-            placeholder={"New York City"}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <TextField
-            variant={"outlined"}
-            fullWidth
-            label={"Link"}
-            value={link}
-            placeholder={"https://application.nycsyep.com/"}
-            onChange={(e) => setLink(e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-          <TextField
-            variant={"outlined"}
-            fullWidth
-            multiline
-            maxRows={10}
-            label={"Description"}
-            value={description}
-            sx={{ marginBottom: "10px" }}
-            placeholder={
-              "Summer Youth Employment Program (SYEP) is the nation’s largest youth employment program, connecting NYC youth between the ages of 14 and 24 with career exploration opportunities and paid work experiences each summer. "
-            }
-            onChange={(e) => setDescription(e.target.value)}
-          />
+        <Grid item xs={6} sm={6} md={2} lg={2} xl={2}>
+          <Grid style={{ height: "100%" }}>
+            <TextField
+              fullWidth
+              label={"Eligibilities"}
+              style={{ height: "100%" }}
+            />
+          </Grid>
         </Grid>
       </Grid>
       <Button
