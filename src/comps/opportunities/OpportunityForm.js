2e5558e5470acaaec28d6d78b3a4636c5a17e44b
YBenPan <ybenp8104@outlook.com>
2022-08-19 18:42:26 -0400

    Add selection form for categories and eligibilities

--- src/comps/opportunities/OpportunityForm.js
+++ src/comps/opportunities/OpportunityForm.js
@@ -1,4 +1,18 @@
-import { Button, Grid, Snackbar, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Chip,
+  Grid,
+  FormControl,
+  InputLabel,
+  ListItemText,
+  OutlinedInput,
+  Select,
+  Snackbar,
+  TextField,
+  MenuItem,
+} from "@mui/material";
 import React from "react";
 import { gql, useMutation } from "@apollo/client";
 
@@ -32,6 +46,31 @@ const CREATE_MUTATION = gql`
   }
 `;
 
+const categories = [
+  "Events of Interest",
+  "Academic Programs",
+  "Business and Jobs",
+  "Community Service",
+  "Leadership, Government, International",
+  "Museums, Art, Design",
+  "Parks, Zoo, Nature",
+  "Engineering, Math, Computer Science",
+  "Medical, Life Sciences",
+  "Theater, Music, Writing, Videos",
+  "Contests, Competitions",
+  "Additional Links and Resources",
+  "Scholarships",
+];
+
+const eligibilities = [
+  "Freshman",
+  "Sophomore",
+  "Junior",
+  "Senior",
+  "Female Only",
+  "Underrepresented Community",
+];
+
 const OpportunityForm = (opportunity = {}) => {
   const [snackbarOpen, setSnackbarOpen] = React.useState(false);
 
@@ -46,15 +85,31 @@ const OpportunityForm = (opportunity = {}) => {
   const [description, setDescription] = React.useState(
     opportunity.description || ""
   );
+  const [allCategory, setAllCategory] = React.useState([]);
+  const [allEligibility, setAllEligibility] = React.useState([]);
+  const handleCategoryChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setAllCategory(value);
+    console.log(allCategory);
+  };
+  const handleEligibilityChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setAllEligibility(value);
+    console.log(allEligibility);
+  };
 
   const [createOpportunity] = useMutation(CREATE_MUTATION);
 
   return (
     <div>
       <Grid container spacing={2} alignItems="stretch">
-        <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
+        <Grid item xs={12} sm={12} md={12} lg={8} xl={8}>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
               <TextField
                 variant={"outlined"}
                 fullWidth
@@ -64,7 +119,7 @@ const OpportunityForm = (opportunity = {}) => {
                 onChange={(e) => setTitle(e.target.value)}
               />
             </Grid>
-            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
               <TextField
                 variant={"outlined"}
                 fullWidth
@@ -74,7 +129,7 @@ const OpportunityForm = (opportunity = {}) => {
                 onChange={(e) => setDate(e.target.value)}
               />
             </Grid>
-            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
               <TextField
                 variant={"outlined"}
                 fullWidth
@@ -84,7 +139,7 @@ const OpportunityForm = (opportunity = {}) => {
                 onChange={(e) => setAppDeadline(e.target.value)}
               />
             </Grid>
-            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
               <TextField
                 variant={"outlined"}
                 fullWidth
@@ -96,7 +151,7 @@ const OpportunityForm = (opportunity = {}) => {
                 }}
               />
             </Grid>
-            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
               <TextField
                 variant={"outlined"}
                 fullWidth
@@ -106,7 +161,7 @@ const OpportunityForm = (opportunity = {}) => {
                 onChange={(e) => setLocation(e.target.value)}
               />
             </Grid>
-            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
               <TextField
                 variant={"outlined"}
                 fullWidth
@@ -134,22 +189,64 @@ const OpportunityForm = (opportunity = {}) => {
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={6} sm={6} md={2} lg={2} xl={2}>
+        <Grid item xs={6} sm={6} md={6} lg={2} xl={2}>
           <Grid style={{ height: "100%" }}>
-            <TextField
-              fullWidth
-              label={"Categories"}
-              style={{ height: "100%" }}
-            />
+            <FormControl fullWidth>
+              <InputLabel id="multiple-categories-label">Categories</InputLabel>
+              <Select
+                labelId="multiple-categories-label"
+                multiple
+                value={allCategory}
+                onChange={handleCategoryChange}
+                input={<OutlinedInput label={"Chip"} />}
+                renderValue={(selected) => (
+                  <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                    {selected.map((value) => (
+                      <Chip key={value} label={value} />
+                    ))}
+                  </Box>
+                )}
+              >
+                {categories.map((category) => (
+                  <MenuItem key={category} value={category}>
+                    <Checkbox checked={allCategory.indexOf(category) > -1} />
+                    <ListItemText primary={category} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </Grid>
         </Grid>
-        <Grid item xs={6} sm={6} md={2} lg={2} xl={2}>
+        <Grid item xs={6} sm={6} md={6} lg={2} xl={2}>
           <Grid style={{ height: "100%" }}>
-            <TextField
-              fullWidth
-              label={"Eligibilities"}
-              style={{ height: "100%" }}
-            />
+            <FormControl fullWidth>
+              <InputLabel id="multiple-eligibilities-label">
+                Eligibilities
+              </InputLabel>
+              <Select
+                labelId="multiple-eligibilities-label"
+                multiple
+                value={allEligibility}
+                onChange={handleEligibilityChange}
+                input={<OutlinedInput label={"Chip"} />}
+                renderValue={(selected) => (
+                  <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                    {selected.map((value) => (
+                      <Chip key={value} label={value} />
+                    ))}
+                  </Box>
+                )}
+              >
+                {eligibilities.map((eligibility) => (
+                  <MenuItem key={eligibility} value={eligibility}>
+                    <Checkbox
+                      checked={allEligibility.indexOf(eligibility) > -1}
+                    />
+                    <ListItemText primary={eligibility} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </Grid>
         </Grid>
       </Grid>
@@ -159,8 +256,10 @@ const OpportunityForm = (opportunity = {}) => {
             variables: {
               title,
               description,
-              categories: [],
-              eligibilities: [],
+              categories: allCategory.map((e) => categories.indexOf(e) + 1),
+              eligibilities: allEligibility.map(
+                (e) => eligibilities.indexOf(e) + 1
+              ),
               date,
               location,
               cost: parseInt(cost) || 0,
