f89326e35cdce2c5607219b2cdd7ac314cc90fc3
David Chen <dchen07@protonmail.com>
2022-08-30 16:07:52 -0400

    Convert Layout modules to `sx`

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,73 @@ import { Helmet } from "react-helmet";
 import UserHome from "../comps/home/UserHome";
 import UnauthenticatedLanding from "../comps/home/UnauthenticatedLanding";
 import UserContext from "../comps/context/UserContext";
-import layout from "../styles/Layout.module.css";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import opportunities from "../img/vector/clip-online-education.svg";
+
+const layout = {
+    container: {
+        minHeight: "25vh",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        maxWidth: 1400,
+        margin: "2rem",
+    },
+    main: {
+        padding: "2.5rem 0.5rem",
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    footer: {
+        width: "100%",
+        height: 100,
+        borderTop: "1px solid #eaeaea",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        minHeight: "25vh",
+        width: "70%",
+        margin: "auto",
+    },
+
+    description: {
+        textAlign: "center",
+        lineHeight: 1.5,
+        fontSize: "1.5rem",
+    },
+
+    cardMedia: {
+        height: 200,
+    },
+
+    card: {
+        width: "100%",
+    },
+
+    imageFit: {
+        objectFit: "contain",
+        maxWidth: "100%",
+        borderRadius: 5,
+    },
+
+    alignLeft: {
+        textAlign: "left",
+    },
+
+    backButtonContainer: {
+        width: "100%",
+    },
+
+    backButtonArrow: {
+        height: 15,
+    },
+};
 
 const Home = () => {
     const user = useContext(UserContext);
@@ -14,8 +78,8 @@ const Home = () => {
             <Helmet>
                 <title>Home</title>
             </Helmet>
-            <div className={layout.container}>
-                <Typography className={layout.title} variant={"h1"}>
+            <Box sx={layout.container}>
+                <Typography sx={layout.title} variant={"h1"}>
                     Welcome to the Stuyvesant Opportunities Bulletin!
                 </Typography>
                 <img
@@ -23,11 +87,10 @@ const Home = () => {
                     alt="Various objects, including a rocket ship, a microscope, and a notebook, pop out of a computer monitor."
                     style={{
                         width: "24rem",
-			height: "18rem"
+                        height: "18rem",
                     }}
                 />
-
-            </div>
+            </Box>
             {user.signedIn ? <UserHome /> : <UnauthenticatedLanding />}
         </div>
     );
