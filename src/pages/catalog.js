129452ca12a6739f29420e074bbae0cea31c8b3c
YBenPan <ybenp8104@outlook.com>
2023-02-15 23:38:53 -0500

    Fix build warning

--- src/pages/catalog.js
+++ src/pages/catalog.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import Typography from "@mui/material/Typography";
 import { Helmet } from "react-helmet";
 import OpportunityList from "../comps/opportunities/OpportunityList";
