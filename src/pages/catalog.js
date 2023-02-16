443de3664c6b9ca79f479ea8871e11f360626f46
YBenPan <ybenp8104@outlook.com>
2023-02-15 21:57:16 -0500

    Implement checkboxes for eligibilities

--- src/pages/catalog.js
+++ src/pages/catalog.js
@@ -6,7 +6,13 @@ import { gql, useQuery } from "@apollo/client";
 import { useLocation, useSearchParams } from "react-router-dom";
 import AuthenticationRequired from "../comps/auth/AuthenticationRequired";
 import UserContext from "../comps/context/UserContext";
-import { CircularProgress } from "@mui/material";
+import {
+  Checkbox,
+  CircularProgress,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+} from "@mui/material";
 
 const QUERY = gql`
   query Opportunities($categories: [Int], $eligibilities: [Int]) {
@@ -41,45 +47,81 @@ const Catalog = () => {
 
   let location = useLocation();
   let categories = location.state?.category ? [location.state?.category] : []; // TODO: add in-page user interface for categories
-  let eligibilities = location.state?.eligibilities
-    ? [location.state?.eligibilities]
-    : [];
+  let eligibilities = [ // TODO: Query instead of declaring as constant
+    "Freshman",
+    "Sophomore",
+    "Junior",
+    "Senior",
+    "Female Only",
+    "Underrepresented Community",
+  ];
 
-  const { data, loading, error } = useQuery(QUERY, {
-    variables: {
-      categories,
-      eligibilities,
-    },
-  });
+  const [allEligibility, setAllEligibility] = React.useState([]); // TODO: Change variable naming
 
-  if (loading || user.loading) return <CircularProgress />;
-  if (!user.signedIn) return <AuthenticationRequired />;
-  if (error) return <p>Error :(</p>;
-
-  let filtered = data["opportunities"];
-  if (searchParams.get("q")) {
-    filtered = data["opportunities"].filter((opportunity) => {
-      for (const key of ["title", "description", "date", "location", "link"]) {
-        if (opportunity[key]?.match(searchParams.get("q"))) {
-          return opportunity;
-        }
-      }
-      return null;
-    });
+  const toggleEligibility = (eligibility) => {
+    const eligibilityIndex = allEligibility.indexOf(eligibility);
+    if (eligibilityIndex === -1) {
+      allEligibility.push(eligibility);
+    } else {
+      allEligibility.splice(eligibilityIndex, 1);
+    }
+    setAllEligibility(allEligibility);
+    console.log(allEligibility);
   }
+
+  // const { data, loading, error } = useQuery(QUERY, {
+  //   variables: {
+  //     categories,
+  //     eligibilities,
+  //   },
+  // });
+
+  // if (loading || user.loading) return <CircularProgress />;
+  // if (!user.signedIn) return <AuthenticationRequired />;
+  // if (error) return <p>Error :(</p>;
+  //
+  // let filtered = data["opportunities"];
+  // if (searchParams.get("q")) {
+  //   filtered = data["opportunities"].filter((opportunity) => {
+  //     for (const key of ["title", "description", "date", "location", "link"]) {
+  //       if (opportunity[key]?.match(searchParams.get("q"))) {
+  //         return opportunity;
+  //       }
+  //     }
+  //     return null;
+  //   });
+  // }
 
   return (
     <div>
       <Helmet>
         <title>Catalog</title>
       </Helmet>
-      <Typography variant={"h1"}>Catalog</Typography>
-      {searchParams && searchParams.get("q") ? (
-        <Typography variant={"h2"}>
-          Search Query: {searchParams.get("q")}
-        </Typography>
-      ) : null}
-      <OpportunityList opportunities={{ opportunities: filtered }} />
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <Typography variant={"h1"}>Catalog</Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={3} lg={2} xl={2}>
+          <FormGroup>
+            {eligibilities.map((eligibility) => (
+              <FormControlLabel
+                control={<Checkbox
+                  onChange={() => toggleEligibility(eligibility)}
+                />}
+                label={eligibility}
+              />
+            ))}
+          </FormGroup>
+        </Grid>
+        <Grid item xs={12} sm={12} md={9} lg={10} xl={10}>
+          {searchParams && searchParams.get("q") ? (
+            <Typography variant={"h2"}>
+              Search Query: {searchParams.get("q")}
+            </Typography>
+          ) : null}
+          {/*<OpportunityList opportunities={{ opportunities: filtered }} />*/}
+        </Grid>
+      </Grid>
     </div>
   );
 };
