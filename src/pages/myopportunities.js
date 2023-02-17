33ade57a1a3cc854161ec02fa5f91ca54e262889
Ben Pan <ybenp8104@outlook.com>
2023-02-16 22:06:30 -0500

    Add my opportunity query

--- src/pages/myopportunities.js
+++ src/pages/myopportunities.js
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useContext } from "react";
 import Typography from "@mui/material/Typography";
 import { Helmet } from "react-helmet";
+import UserContext from "../comps/context/UserContext";
+import { CircularProgress } from "@mui/material";
+import { gql, useQuery } from "@apollo/client";
+import AuthenticationRequired from "../comps/auth/AuthenticationRequired";
+import OpportunityList from "../comps/opportunities/OpportunityList";
+
+const QUERY = gql`
+  query Opportunities($user: [Int]) {
+    opportunities(user: $user) {
+      id
+      title
+      description
+      categories {
+        id
+        name
+        description
+      }
+      eligibilities {
+        id 
+        name
+        description
+      }
+      date
+      location
+      cost
+      appDeadline
+      link
+    }
+  }
+`;
 
 const MyOpportunities = () => {
+  const user = useContext(UserContext);
+  const userId = user.id;
+
+  const { data, loading, error } = useQuery(QUERY, {
+    variables: {
+      user: userId
+    },
+  });
+
+  if (loading || user.loading) return <CircularProgress />;
+  if (!user.signedIn) return <AuthenticationRequired />;
+  if (error) {
+    console.log(error);
+    return <p>Error :(</p>;
+  }
+  let filtered = data["opportunities"];
+
   return (
     <div>
       <Helmet>
         <title>My Opportunities</title>
       </Helmet>
-      <main>
-        <Typography paragraph>My opportunities page</Typography>
-      </main>
+      <Typography variant={"h1"}>My opportunities</Typography>
+      <OpportunityList opportunities={{ opportunities: filtered }} />
     </div>
   );
 };
