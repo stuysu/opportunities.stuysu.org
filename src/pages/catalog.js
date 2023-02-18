fdd3671455a3e2a188c66e6a01450ec1c5e32ead
YBenPan <ybenp8104@outlook.com>
2023-02-18 13:24:26 -0500

    Add cost input

--- src/pages/catalog.js
+++ src/pages/catalog.js
@@ -1,17 +1,19 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import Typography from "@mui/material/Typography";
 import { Helmet } from "react-helmet";
 import OpportunityList from "../comps/opportunities/OpportunityList";
-import { gql, useQuery } from "@apollo/client";
+import {gql, useLazyQuery, useQuery} from "@apollo/client";
 import { useLocation, useSearchParams } from "react-router-dom";
 import AuthenticationRequired from "../comps/auth/AuthenticationRequired";
 import UserContext from "../comps/context/UserContext";
 import {
+  Button,
   Checkbox,
   CircularProgress,
   FormControlLabel,
   FormGroup,
-  Grid,
+  Grid, Input,
+  Slider,
 } from "@mui/material";
 
 
@@ -24,8 +26,8 @@ const ELIGIBILITY_QUERY = gql`
 `;
 
 const QUERY = gql`
-  query Opportunities($categories: [Int], $eligibilities: [Int]) {
-    opportunities(categories: $categories, eligibilities: $eligibilities) {
+  query Opportunities($cost: Int, $categories: [Int], $eligibilities: [Int]) {
+    opportunities(cost: $cost, categories: $categories, eligibilities: $eligibilities) {
       id
       title
       description
@@ -50,6 +52,7 @@ const QUERY = gql`
 
 const Catalog = () => {
   const user = useContext(UserContext);
+  const [maxCost, setMaxCost] = useState(10000);
 
   // uses ? parameters as search params, targeting `q` as the search engine query key
   let [searchParams] = useSearchParams(); // TODO: filter data server-side in the GraphQL query
@@ -57,16 +60,46 @@ const Catalog = () => {
   let location = useLocation();
   let categories = location.state?.category ? [location.state?.category] : []; // TODO: add in-page user interface for categories
 
+  /**
+   * Toggles eligibility
+   * @param {string} eligibility - the name of the eligibility item to be toggled on/off
+   */
+  const toggleEligibility = (eligibility) => {
+    const newEligibilities = [...eligibilities];
+    const eligibilityIndex = eligibilities.indexOf(eligibility);
+    if (eligibilityIndex === -1) {
+      newEligibilities.push(eligibility);
+    } else {
+      newEligibilities.splice(eligibilityIndex, 1);
+    }
+    setEligibilities(newEligibilities);
+    console.log(newEligibilities);
+  };
+
+  // TODO: Fix Slider and re-rendering issues
+  // const handleSliderChange = (event, newCost) => {
+  //   setMaxCost(newCost);
+  //
+  // }
+  //
+  // const handleSliderChangeCommitted = (event, newCost) => {
+  // };
+
+  const handleInputChange = (event) => {
+    setMaxCost(event.target.value === '' ? '' : Number(event.target.value))
+  }
+
   // Get array of eligibility names
   const eligibilities_response = useQuery(ELIGIBILITY_QUERY);
   const allEligibilities = eligibilities_response?.data?.eligibilities?.map(a => a.name);
   const [eligibilities, setEligibilities] = React.useState();
   const { data, loading, error } = useQuery(QUERY, {
     variables: {
-      categories,
+      cost: maxCost,
+      categories: categories,
       eligibilities: eligibilities?.map((e) => allEligibilities?.indexOf(e) + 1),
     },
-    skip: eligibilities_response.loading || !allEligibilities
+    skip: eligibilities_response.loading || !allEligibilities,
   });
   useEffect(() => {
       if (eligibilities === undefined) {
@@ -90,22 +123,6 @@ const Catalog = () => {
     });
   }
 
-  /**
-   * Toggles eligibility
-   * @param {string} eligibility - the name of the eligibility item to be toggled on/off
-   */
-  const toggleEligibility = (eligibility) => {
-    const newEligibilities = [...eligibilities];
-    const eligibilityIndex = eligibilities.indexOf(eligibility);
-    if (eligibilityIndex === -1) {
-      newEligibilities.push(eligibility);
-    } else {
-      newEligibilities.splice(eligibilityIndex, 1);
-    }
-    setEligibilities(newEligibilities);
-    console.log(newEligibilities);
-  };
-
   return (
     <div>
       <Helmet>
@@ -127,6 +144,38 @@ const Catalog = () => {
               />
             ))}
           </FormGroup>
+          <Typography id="cost-slider">
+            Max Cost
+          </Typography>
+          <Grid container spacing={2} alignItems="center">
+            {/*<Grid item xs={12} sm={8} md={8} lg={8} xl={8}>*/}
+            {/*  <Slider*/}
+            {/*    value={maxCost}*/}
+            {/*    min={0}*/}
+            {/*    max={10000}*/}
+            {/*    step={1000}*/}
+            {/*    valueLabelDisplay="auto"*/}
+            {/*    onChange={handleSliderChange}*/}
+            {/*    onChangeCommitted={handleSliderChangeCommitted}*/}
+            {/*  />*/}
+            {/*</Grid>*/}
+            <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
+              <Input
+                autoFocus
+                value={maxCost}
+                size="small"
+                onChange={handleInputChange}
+              />
+            </Grid>
+            {/*<Button*/}
+            {/*  variant="contained"*/}
+            {/*  onClick={() => {*/}
+            {/*    query();*/}
+            {/*  }}*/}
+            {/*>*/}
+            {/*  Submit*/}
+            {/*</Button>*/}
+          </Grid>
         </Grid>
         <Grid item xs={12} sm={12} md={9} lg={10} xl={10}>
           {searchParams && searchParams.get("q") ? (
