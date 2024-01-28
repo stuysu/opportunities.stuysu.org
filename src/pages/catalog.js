bace481e8ac1414b2ae44c20130f7b3d4116f7e9
David Chen <dchen07@protonmail.com>
2024-01-28 16:29:01 -0500

    Fix category filter bug

--- src/pages/catalog.js
+++ src/pages/catalog.js
@@ -214,6 +214,7 @@ const Catalog = () => {
 
   const setCategoriesWrapper = (categories) => {
     window.sessionStorage.setItem("categories", JSON.stringify(categories));
+    setOpportunities([]);
     setCategories(categories);
   };
 
